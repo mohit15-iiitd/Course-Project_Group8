a0d61830b35bd54692a0181ba4a7d2fd24a0ab43
akshranair <68681614+akshranair@users.noreply.github.com>
2023-03-10 01:32:51 +0530

    Update Text Preprcessing.py

--- Text Preprcessing.py
+++ Text Preprcessing.py
@@ -61,26 +61,38 @@ data
 
 
 # preprocessing funciton created
+# preprocessing funciton created
 def preprocessing(df):
     df1 = df.copy()
     df1['white_space_removed'] = 0
+    df1['numbers_removed']=0
+    df1['url_removed_data'] = 0
+    df1['user_removed'] = 0
     df1['emoji_removed'] = 0
     df1['tokenized_data'] = 0
     df1['stopword_removed_data'] = 0
     df1['punct_removed_data'] = 0
-    df1['url_removed_data'] = 0
-    df1['lemma_data'] = 0
     df1['spelling_checked_data'] = 0
+    df1['lemma_data'] = 0
     stop_words = set(stopwords.words("english"))
 
+#   lemmatizer = WordNetLemmatizer()
     spell = Speller(lang='en')
 
 
-    for i in range(df1.shape[0]):
+  # iterate over each row of dataset and preprocess data
+    for i in tqdm(range(df1.shape[0])):
 
         # white space removel
         df1['white_space_removed'][i] = re.sub("\s+", " ", df1.text[i])
-        emoji_removed = re.sub(r'\W+', ' ', df1['white_space_removed'][i].encode('ascii', 'ignore').decode('utf-8'), flags=re.UNICODE).strip()
+        #REMOVING URL
+        df1['url_removed_data'][i] = re.sub('(www|http)\S+'," ",df1['white_space_removed'][i])
+        #REMOVING USERS
+        df1['user_removed'][i] = re.sub('^@\S+'," ",df1['url_removed_data'][i])
+        #REMOVING NUMBERS
+        df1['numbers_removed'][i] = re.sub('\d+','',df1['user_removed'][i])
+        #REMOVING EMOJIS
+        emoji_removed = re.sub(r'\W+', ' ', df1['numbers_removed'][i].encode('ascii', 'ignore').decode('utf-8'), flags=re.UNICODE).strip()
         df1['emoji_removed'][i] = emoji_removed
 
         # lower casing and tokenization
@@ -88,34 +100,29 @@ def preprocessing(df):
         tokenized_data = word_tokenize(lower)
         df1['tokenized_data'][i] = tokenized_data
         # print(tokenized_data)
+        
+        
 
 
         # remove stopwords
         stopword_removed_data = [x for x in tokenized_data if x not in stop_words]
         df1['stopword_removed_data'][i] = stopword_removed_data 
         # print(stopword_removed_data)
-
-
-        # remove urls and html tags
-        urls = re.findall("https?://[a-zA-Z0-9_\?=\@\/#=.~-]+", " ".join(stopword_removed_data))
-        url_removed_data = [x for x in stopword_removed_data if x not in urls]
-        df1['url_removed_data'][i] = url_removed_data
-        # print(url_removed_data)
-        # print(type(url_removed_data))
         
         
-        # punctuation removel
-        punct_removed_data = [x for x in url_removed_data if x.isalnum()]
+        # punctuation removal
+        punct_removed_data = [x for x in stopword_removed_data if x.isalnum()]
         df1['punct_removed_data'][i] = punct_removed_data
         # print(punct_removed_data)
         
 
         spelling_checked_data = [spell(x) for x in punct_removed_data]
-        df1['spelling_checked_data'][i] = " ".join(spelling_checked_data)
+        df1['spelling_checked_data'][i] = spelling_checked_data
 #         print(" ".join(spelling_checked_data))
 
         lemma_data = [lemma.lemmatize(x) for x in spelling_checked_data]
-        df1['lemma_data'][i] = lemma_data
+        df1['lemma_data'][i] = " ".join(lemma_data)
+
         # spelling checking
 
     return df1
